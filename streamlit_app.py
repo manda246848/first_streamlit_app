2a134d0d9280f3c3f922a65ba23922e87824dfa2
manda246848 <139783455+manda246848@users.noreply.github.com>
2023-08-07 15:42:51 +0530

    Update streamlit_app.py
    updated images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega3 & BlueBerry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega3 & BlueBerry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
